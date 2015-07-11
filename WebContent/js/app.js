b633c8895b444ce847fc09c66d1a48b1a7d50755
Anıl Sevici <anilsevici@hotmail.com>
2015-07-11 19:55:17 +0300

    Fix

---
+++ WebContent/js/app.js
@@ -0,0 +1,254 @@
+var app = angular.module("app", [ "ui.router", "ui.bootstrap",
+		"ui.bootstrap.datetimepicker", "ngCookies" ]);
+app.config([ '$stateProvider', '$urlRouterProvider', '$locationProvider',
+		function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+			// For any unmatched url, redirect to /state1
+			$urlRouterProvider.otherwise("/home");
+			//
+			// Now set up the states
+			$stateProvider.state('home', {
+				url : "/home",
+				templateUrl : "home.html",
+				controller : 'HomeCtrl'
+			}).state('login', {
+				url : "/login",
+				templateUrl : "login.html",
+				controller : 'LoginCtrl'
+			}).state('ilan', {
+				url : "/ilan",
+				templateUrl : "ilan.html",
+				controller : 'IlanCtrl'
+			})
+		} ]);
+
+app
+		.factory(
+				'AuthService',
+				function($http, $q, $cookies, Session) {
+					var login = function(credentials) {
+						var def = $q.defer();
+						$http(
+								{
+									method : 'POST',
+									url : 'IKServlet',
+									headers : {
+										'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+									},
+									params : {
+										user : credentials
+									}
+								}).success(
+								function(data, status, headers, config) {
+
+								}).error(function(error) {
+							// called asynchronously if an error occurs
+							// or server returns response with an error status.
+							def.reject(error);
+						});
+						return def.promise;
+					};
+					var isAuthenticated = function() {
+						return $cookies.get('name');
+					};
+
+					var getUsername = function() {
+						return $cookies.get('name');
+					};
+
+					var logout = function() {
+						var deferred = $q.defer();
+						Session.destroy();
+						return deferred.promise;
+					};
+					return {
+						login : login,
+						isAuthenticated : isAuthenticated,
+						logout : logout,
+						getUsername : getUsername
+					};
+
+				});
+
+app.factory('Session', function() {
+	this.create = function(auth, username) {
+		this.auth = auth;
+		this.username = username;
+	};
+	this.destroy = function() {
+		this.auth = false;
+		this.username = null;
+	};
+	return this;
+});
+
+app.controller('LoginCtrl', function($scope, $location, AuthService) {
+	$scope.credentials = {
+		username : '',
+		password : ''
+	};
+	$scope.login = function(credentials) {
+		AuthService.login(credentials).then(function(username) {
+			$scope.setErrorMessage(null);
+			$scope.setCurrentUsername(username);
+			$location.path('/ilan');
+		}, function(response) {
+			$window.alert("Invalid credentials");
+			// $scope.setErrorMessage(response.data.error.message);
+		});
+	};
+});
+
+app.controller('IlanCtrl', function($scope, $http) {
+	$scope.ilan = {
+		ilanid : '',
+		title : '',
+		definition : '',
+		description : '',
+		aktif : '',
+		pasif : '',
+
+	};
+
+	$scope.setilanDate = function(value) {
+		$scope.ilan.aktif = value.toLocaleString();
+	};
+
+	$scope.setilanDate2 = function(value) {
+		$scope.ilan.pasif = value.toLocaleString();
+		date: ''
+	};
+
+	$scope.setilanDate = function(value) {
+		$scope.ilan.date = value;
+	};
+
+	$scope.saveilan = function(ilan) {
+
+		$http({
+			method : 'GET',
+			url : 'IlanServlet',
+			params : {
+				ilan : ilan
+			}
+
+		}).success(function(data, status, headers, config) {
+
+		}).error(function(error) {
+			// called asynchronously if an error occurs
+			// or server returns response with an error status.
+		});
+
+	};
+
+});
+
+app.controller('DateTimePickerDemoCtrl',
+		function($scope, $timeout) {
+			$scope.dateTimeNow = function() {
+				$scope.date = new Date();
+
+				$scope.date2 = new Date();
+
+			};
+			$scope.dateTimeNow();
+
+			$scope.toggleMinDate = function() {
+				$scope.minDate = $scope.minDate ? null : new Date();
+			};
+
+			$scope.maxDate = new Date('2014-06-22');
+			$scope.toggleMinDate();
+
+			$scope.dateOptions = {
+				startingDay : 1,
+				showWeeks : false
+			};
+
+			// Disable weekend selection
+			$scope.disabled = function(calendarDate, mode) {
+				return mode === 'day'
+						&& (calendarDate.getDay() === 0 || calendarDate
+								.getDay() === 6);
+			};
+
+			$scope.hourStep = 1;
+			$scope.minuteStep = 15;
+
+			$scope.timeOptions = {
+				hourStep : [ 1, 2, 3 ],
+				minuteStep : [ 1, 5, 10, 15, 25, 30 ]
+			};
+
+			$scope.showMeridian = true;
+			$scope.timeToggleMode = function() {
+				$scope.showMeridian = !$scope.showMeridian;
+			};
+
+			$scope.$watch("date", function(value) {
+				console.log('New date value:' + value);
+				$scope.setilanDate(value);
+			}, true);
+
+			$scope.$watch("date2", function(value) {
+				console.log('New date value:' + value);
+				$scope.setilanDate2(value);
+			}, true);
+
+			$scope.resetHours = function() {
+				$scope.date.setHours(1);
+				$scope.date2.setHours(1);
+			};
+		});
+
+app.controller('HomeCtrl', function($scope, $http, $timeout) {
+
+	$scope.ilanlar = [];
+
+	$scope.onTimeout = function() {
+
+		$http({
+			method : 'GET',
+			url : 'IlanGorServlet',
+
+		}).success(function(data, status, headers, config) {
+			$scope.ilanlar = data;
+
+		}).error(function(error) {
+			// called asynchronously if an error occurs
+			// or server returns response with an error status.
+		});
+
+		mytimeout = $timeout($scope.onTimeout, 120000);
+
+	}
+
+	var mytimeout = $timeout($scope.onTimeout, 120000);
+	$scope.onTimeout();
+
+	$scope.toggleIlanDetails = function(e, index) {
+		if ($scope.index == index) {
+			delete $scope.index;
+		} else {
+			$scope.index = index;
+		}
+		e.preventDefault();
+
+	};
+
+});
+
+app.controller('AppCtrl', function($scope, AuthService) {
+	$scope.currentUsername = null;
+	$scope.isAuthenticated = AuthService.isAuthenticated;
+	$scope.setErrorMessage = function(message) {
+		$scope.errorMessage = message;
+	};
+	$scope.setCurrentUsername = function(username) {
+		$scope.currentUsername = username;
+	};
+	$scope.logout = function() {
+		AuthService.logout();
+	}
+
+});
---
+++ src/main/java/com/anilsevici/accountik/IKServlet.java
@@ -0,0 +1,65 @@
+package com.anilsevici.accountik;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+/**
+ * Servlet implementation class IKServlet
+ */
+@WebServlet("/IKServlet")
+public class IKServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public IKServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String data = request.getParameter("user");
+
+		JsonParser parser = new JsonParser();
+		JsonObject joUser = parser.parse(data).getAsJsonObject();
+
+		String name = joUser.get("username").getAsString();
+		String password = joUser.get("password").getAsString();
+
+		LdapAccounting con = new LdapAccounting(name, password);
+		try {
+			con.connect();
+
+			Cookie ck = new Cookie("name", name);
+			response.addCookie(ck);
+		} catch (Exception e) {
+
+		}
+	}
+
+}
